ff135d24b519eb776fe966e472483c140a47fff2
toxypiks <HannaHinkelbein89@gmx.de>
2026-08-25 22:23:17 +0200

    Increase tone by two half-tones each second

--- src/main.c
+++ src/main.c
@@ -7,14 +7,13 @@
 #define SAMPLE_RATE 44100
 #define SAMPLE_SIZE 32
 #define CHANNELS 1
-#define BUFFER_SIZE 1024
 
 int main(void)
 {
     InitWindow(800, 600, "synth");
     InitAudioDevice();
 
-    float buffer[BUFFER_SIZE];
+    float buffer[1024];
     SetAudioStreamBufferSizeDefault(ARRAY_LEN(buffer));
 
     AudioStream synth = LoadAudioStream(SAMPLE_RATE, SAMPLE_SIZE, CHANNELS);
@@ -22,7 +21,8 @@ int main(void)
     PlayAudioStream(synth);
     SetTargetFPS(60);
     int synth_frame_count = 0;
-    float synth_freq = 440.0;
+    float next_semitone = pow(2.0, 1.0/12.0);
+    float synth_freq = 440.0*next_semitone;
 
     while(!WindowShouldClose()) {
 
@@ -33,10 +33,10 @@ int main(void)
                 for(int i = 0; i < ARRAY_LEN(buffer); ++i)
                 {
                     float time = (float)synth_frame_count/SAMPLE_RATE;
-                    buffer[i] = sin(2*M_PI*time*440);
+                    buffer[i] = sin(2*M_PI*time*synth_freq*pow((next_semitone*next_semitone), floorf(GetTime())));
                     synth_frame_count += 1;
                 }
-                UpdateAudioStream(synth, buffer, BUFFER_SIZE);
+                UpdateAudioStream(synth, buffer, ARRAY_LEN(buffer));
             }
             EndDrawing();
         }
